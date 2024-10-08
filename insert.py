518a67b18ce7beca8679e58f265370046b264e87
nuGhinTanjid <tanjidit2@gmail.com>
2024-10-09 02:34:53 +0600

    three

---
+++ insert.py
@@ -0,0 +1,22 @@
+import mysql.connector
+db_name ="python_test_db"
+
+mydbconnection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+    database=db_name
+)
+
+mycursor = mydbconnection.cursor()
+
+sqlquery = """ 
+            INSERT INTO Student
+            (Roll,  Name)
+            VALUES ('222','tANJ')
+            """
+
+mycursor.execute(sqlquery) 
+mydbconnection.commit()
+
+print("data inserted!")
